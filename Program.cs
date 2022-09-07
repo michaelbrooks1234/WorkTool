7af6e6fc920b625e7f12b462ee0b98cc54ef6357
Michael <michaelbrooks6897@gmail.com>
2022-09-07 01:20:34 -0400

    starting code for work related tools

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+namespace System;
+
+class Program2
+{
+    static void Main()
+    {
+        using HttpClient client = new HttpClient();
+
+        string path = Directory.GetCurrentDirectory();
+        string[] credentials = System.IO.File.ReadAllLines(@path+"/src/startupConfig.txt");
+        Dictionary<string, string> credentialsDict = GetCredentials(credentials);
+
+        Console.WriteLine(credentialsDict["user"]);
+    }
+
+    static Dictionary<string,string> GetCredentials(string[] credentials)
+    {
+        Dictionary<string, string> parsedCredentials = new Dictionary<string, string>();
+        for(int i = 0; i < credentials.Length; i++){
+            string[] splitCreds = credentials[i].Split("=");
+            parsedCredentials.Add(splitCreds[0], splitCreds[1]);
+        }
+        return parsedCredentials;
+    }
+}
