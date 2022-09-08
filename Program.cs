b28cccb9c8acc7b391e003d4b7c603660376e02c
Michael <michaelbrooks6897@gmail.com>
2022-09-08 00:08:24 -0400

    testing git ignore

--- Program.cs
+++ Program.cs
@@ -9,18 +9,23 @@ class Program2
 
         string path = Directory.GetCurrentDirectory();
         string[] credentials = System.IO.File.ReadAllLines(@path+"/src/startupConfig.txt");
+
         Dictionary<string, string> credentialsDict = GetCredentials(credentials);
 
-        Console.WriteLine(credentialsDict["user"]);
+        
+
     }
 
     static Dictionary<string,string> GetCredentials(string[] credentials)
     {
+
         Dictionary<string, string> parsedCredentials = new Dictionary<string, string>();
+
         for(int i = 0; i < credentials.Length; i++){
             string[] splitCreds = credentials[i].Split("=");
             parsedCredentials.Add(splitCreds[0], splitCreds[1]);
         }
+
         return parsedCredentials;
     }
 }
